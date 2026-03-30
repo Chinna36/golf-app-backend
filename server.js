c0acd3b1c6926fc7994bbc5d355f430d9a3c063e
Chinna36 <chinnaneelam02@gmail.com>
2026-03-30 20:49:28 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.get("/api/scores", (req, res) => {
+  res.json([
+    { id: 1, score: 20 },
+    { id: 2, score: 40 },
+    { id: 3, score: 60 }
+  ]);
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
